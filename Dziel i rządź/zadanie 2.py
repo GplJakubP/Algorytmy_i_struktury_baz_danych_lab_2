fcf257bb065d42ff0a565e5073ed37c2138350d2
GplJakubP <115486348+GplJakubP@users.noreply.github.com>
2023-06-08 18:18:06 +0200

    Update zadanie 2.py

--- Dziel i rządź/zadanie 2.py
+++ Dziel i rządź/zadanie 2.py
@@ -1,18 +1,26 @@
-def create_array(i, j):
-    array = [[0] * (j + 1) for _ in range(i + 1)]
-    for x in range(i + 1):
-        for y in range(j + 1):
-            if x > 0 and y == 0:
-                array[x][y] = 0
-            elif x == 0 and y > 0:
-                array[x][y] = 1
-            elif x > 0 and y > 0:
-                array[x][y] = round((array[x - 1][y] + array[x][y - 1]) / 2, 3)
-
-    return array
-n = 5
-i = 5
-j = 5
-result = create_array(i, j)
-for row in result:
-    print([round(val, 2) for val in row])
+def merge(l, m, r, A, B):
+    i, j, k = l, m, l
+    while i < m and j <= r:
+        if A[i] <= A[j]:
+            B[k] = A[i]
+            i += 1
+        else:
+            B[k] = A[j]
+            j += 1
+        k += 1
+    while i < m:
+        B[k] = A[i]
+        i += 1
+        k += 1
+    while j <= r:
+        B[k] = A[j]
+        j += 1
+        k += 1
+def main():
+    A = [1, 35, 42, 65, 68, 25, 55, 59, 70, 79]
+    B = [0] * len(A)
+    merge(0, 5, 9, A, B)
+    for b in B:
+        print(b, end=' ')
+if __name__ == '__main__':
+    main()
