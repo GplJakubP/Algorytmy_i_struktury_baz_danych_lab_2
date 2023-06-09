57136998c70493a411dd53330089ed775cf1937e
GplJakubP <115486348+GplJakubP@users.noreply.github.com>
2023-06-09 09:25:35 +0200

    Update programowanie dynamiczne zadanie 3.py

--- PROGRAMOWANIE DYNAMICZNE/programowanie dynamiczne zadanie 3.py
+++ PROGRAMOWANIE DYNAMICZNE/programowanie dynamiczne zadanie 3.py
@@ -7,7 +7,7 @@ def obliczn(n):
             return Sn[n]
         Sn[n-1] = obliczn(n-1)
         Sn[n-2] = obliczn(n-2)
-        Sn[n] = Sn[n-1] + Sn[n-2]
+        Sn[n] = 2*Sn[n-1] + Sn[n-2]
         return Sn[n]
 n = int(input("Podaj wartość n: "))
 result = obliczn(n)
